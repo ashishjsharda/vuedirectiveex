8a41943b9316de69f5574be61a0dd25b7c659c42
Ashish Sharda <ashishjsharda@gmail.com>
2022-12-18 05:40:21 -0600

    add template to app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LoopOver />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoopOver from "@/components/LoopOver";
 
 export default {
-  name: 'App',
+
   components: {
-    HelloWorld
+    LoopOver
   }
 }
 </script>
