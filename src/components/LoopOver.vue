1c1319e9b12a91db5b07fab5409050d808eef380
Ashish Sharda <ashishjsharda@gmail.com>
2022-12-18 05:38:03 -0600

    add loop over

---
+++ src/components/LoopOver.vue
@@ -0,0 +1,30 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      interests:['TV','Movies','Music','Books','Sports','Food','Travel','Fashion','Gaming','Art','Photography','Science','Technology','Nature','Animals','Cars','Fitness','Health','History','Politics','Religion','Spirituality','Family','Friends','Work','Money','Education','Philosophy','Psychology','Relaxation','Humor','Other'],
+    }
+  },
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
